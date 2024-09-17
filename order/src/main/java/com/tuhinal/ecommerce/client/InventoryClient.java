3aa5557b5e2650810a24d950672961652a7f3da5
Tuhin <alauddintuhin1411@gmail.com>
2024-09-17 16:12:07 +0600

    added inventory client in order service for testing circuit breaker, added kafka docker configuration

--- order/src/main/java/com/tuhinal/ecommerce/client/InventoryClient.java
+++ order/src/main/java/com/tuhinal/ecommerce/client/InventoryClient.java
@@ -2,15 +2,26 @@ package com.tuhinal.ecommerce.client;
 
 import com.tuhinal.ecommerce.client.req_dto.InventoryCheckDto;
 import com.tuhinal.ecommerce.utils.ApiResponse;
-import org.springframework.cloud.openfeign.FeignClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(name = "inventory", url = "http://localhost:9020")
+//@FeignClient(name = "inventory", url = "http://localhost:9020")
 public interface InventoryClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory/check")
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
+
+    @GetExchange("/api/inventory/check")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     ResponseEntity<ApiResponse<Boolean>> isInStock(@RequestBody InventoryCheckDto inventoryCheckDto);
+
+    default boolean fallbackMethod(String code, Integer quantity, Throwable throwable) {
+        log.info("Cannot get inventory for skuCode {}, failure reason {}", code, throwable.getMessage());
+        return false;
+    }
 }
