fc95190615ed474d4ecc22383d760738af559818
Tuhin <md.alauddin@bjitgroup.com>
2024-09-17 08:06:40 +0600

    implementing API Gateway Router for microservices using Resilience4j

---
+++ gateway/src/main/java/com/tuhinal/ecommerce/gateway/routes/Routes.java
@@ -0,0 +1,77 @@
+package com.tuhinal.ecommerce.gateway.routes;
+
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
+
+@Configuration
+public class Routes {
+    /**
+     * Functional Endpoint Programming Model
+     */
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceSearchRoute() {
+        return GatewayRouterFunctions
+                .route("product_service")
+                .route(RequestPredicates.path("/api/product/search"), HandlerFunctions.http("http://localhost:9010"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceSaveRoute() {
+        return GatewayRouterFunctions
+                .route("product_service")
+                .route(RequestPredicates.path("/api/product"), HandlerFunctions.http("http://localhost:9010"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceSaveRoute() {
+        return GatewayRouterFunctions
+                .route("order_service")
+                .route(RequestPredicates.path("/api/order"), HandlerFunctions.http("http://localhost:9020"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("orderServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
+                .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceSearchRoute() {
+        return GatewayRouterFunctions
+                .route("order_service")
+                .route(RequestPredicates.path("/api/order/search"), HandlerFunctions.http("http://localhost:9020"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("orderServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
+                .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute() {
+        return GatewayRouterFunctions
+                .route("inventory_service")
+                .route(RequestPredicates.path("/api/inventory/check"), HandlerFunctions.http("http://localhost:9030"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("inventoryServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute() {
+        return GatewayRouterFunctions
+                .route("fallbackRoute")
+                .GET("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .body("Service is Unavailable, Please try again later."))
+                .build();
+    }
+}
